6e7e41eaf21913f0729bbe4f0366de6ac0ee047f
YuiSurbhi <163987279+YuiSurbhi@users.noreply.github.com>
2024-05-01 16:38:46 +0000

    Q3 commited

---
+++ Q_3.py
@@ -0,0 +1,16 @@
+# Programof square root.
+
+'''program with exponentiation'''
+num = int(input("Enter the number : "));
+
+sq = num**(1/2);
+
+print("The square of given number is : ",sq);
+
+'''program with maths module'''
+import math
+num = int(input("Enter the number : "));
+
+sq = math.sqrt(num);
+
+print("The square of given number is : ",sq);
