dc131755a505613de229ea2aa8d57b3c74ca3f58
YuiSurbhi <163987279+YuiSurbhi@users.noreply.github.com>
2024-05-01 17:51:42 +0000

    Q10 commited

---
+++ Q_10.py
@@ -0,0 +1,14 @@
+# program to find largest amoung three numbers.
+
+num1 = 32;
+num2 = 45;
+num3 = 55;
+
+if(num1 > num2) and (num1 > num3):
+    print(num1,"is a largest number.");
+
+elif(num2 > num1) and (num2 > num3):
+    print(num2,"is a largest number.");
+
+else:
+    print(num3,"is a largest number.");
