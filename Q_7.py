dca40f68160f914aef677f5e46c5887367d10b80
YuiSurbhi <163987279+YuiSurbhi@users.noreply.github.com>
2024-05-01 17:33:03 +0000

    Q7 commited

---
+++ Q_7.py
@@ -0,0 +1,12 @@
+# program to check if number is posiive, negatiove or 0.
+
+num = float(input("Enter the number : "));
+
+if num > 0:
+    print("Number is positive.");
+
+elif num == 0:
+    print("It is zero.");
+
+else:
+    print("The number is negative.");
