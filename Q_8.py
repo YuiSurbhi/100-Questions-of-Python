4ba9001444aee0550cd83577355a9e720bfe8d99
YuiSurbhi <163987279+YuiSurbhi@users.noreply.github.com>
2024-05-01 17:37:00 +0000

    Q8 commited

---
+++ Q_8.py
@@ -0,0 +1,9 @@
+# Program to check if a number is even or odd.
+
+num = int(input("Enter the number : "));
+
+if num % 2 == 0:
+    print("The number is even.");
+
+else:
+    print("The numer id odd.");
