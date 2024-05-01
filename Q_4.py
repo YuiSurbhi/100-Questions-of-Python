26a39793253958a66567b85d519f907b34a6f796
YuiSurbhi <163987279+YuiSurbhi@users.noreply.github.com>
2024-05-01 16:50:58 +0000

    Q4 commited

---
+++ Q_4.py
@@ -0,0 +1,8 @@
+# Program to calculate the area of a triangle.
+
+height = float(input("Enter the height of triangle : "));
+base = float(input("Enter the base of triangle : "));
+
+area = 0.5*base*height;
+
+print("The are of trangle is : ",area);
