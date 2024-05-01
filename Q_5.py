1d249bd2a8340f2c452ae952053a513d3920b17a
YuiSurbhi <163987279+YuiSurbhi@users.noreply.github.com>
2024-05-01 16:56:50 +0000

    Q5 commited

--- Q_5.py
+++ Q_5.py
@@ -12,3 +12,12 @@ print("The value of x variable is : ",x);
 
 y = temp;
 print("The value of y variable is : ",y);
+
+'''program without using third variable'''
+x = 12;
+y = 13;
+
+x,y = y,x;
+
+print("The value of x variable is : ",x);
+print("The value of y variable is : ",y);
