fb3b92ba06d552146becc2d172c86c5a221517a9
YuiSurbhi <163987279+YuiSurbhi@users.noreply.github.com>
2024-05-02 13:10:43 +0000

    Q14 commited

---
+++ Q_14.py
@@ -0,0 +1,7 @@
+# Program to covert celcius into fahrenheit.
+
+celcius = int(input("Enter the temprature in celcius : "));
+
+fahrenheit = (celcius*(9/5))+32;
+
+print("The coverted value is :",fahrenheit,"fahrenheit");
