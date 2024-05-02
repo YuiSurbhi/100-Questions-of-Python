fcddad2c3ddc00e25436ee264041489220411e00
YuiSurbhi <163987279+YuiSurbhi@users.noreply.github.com>
2024-05-02 12:45:20 +0000

    Q4 commited

---
+++ Q_11.py
@@ -0,0 +1,13 @@
+# Program to check a number is prime or not.
+
+num = int(input("Enter the number : "));
+
+if num == 1:
+    print("It is not a prime number.");
+if num > 1:
+    for i in range (2,num):
+        if num % 2 == 0:
+            print("It is not a prime number.");
+            break
+    else:
+        print("It is a prime number.");
