cad3e96911df42707296fd04973fb87b96e3c4dd
YuiSurbhi <163987279+YuiSurbhi@users.noreply.github.com>
2024-05-02 13:58:43 +0000

    Q15 commited

--- Q_15.py
+++ Q_15.py
@@ -14,3 +14,14 @@ if num > 0:
     for i in range (1,num + 1):
         fact = fact*i;
 print("The factorial of a given number is : ",fact);
+
+'''program using recursion'''
+def fact(a):
+    if a == 0:
+        return 1;
+    else:
+        return((a)*fact(a - 1));
+num = int(input("Enter the number : "));
+
+result = fact(num);
+print("The factorial of given number is : ",result);
