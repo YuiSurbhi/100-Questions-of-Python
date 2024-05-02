5a3ff97051111a34e988d6b757fcacef0f2a7805
YuiSurbhi <163987279+YuiSurbhi@users.noreply.github.com>
2024-05-02 14:04:27 +0000

    Q16 commited

--- Q_16.py
+++ Q_16.py
@@ -1,1 +1,16 @@
-# Program 
+# Program to print multiplication table.
+
+'''program using for loop'''
+num = int(input("Enter the number : "));
+
+for i in range(1,11):
+    print(num,"X",i,"=",num*i);
+
+'''program using while loop'''
+num = int(input("Enter the number : "));
+
+i = 1;
+while i<=10:
+    print(num,"X",i,"=",num*i);
+    
+    i = i + 1;
