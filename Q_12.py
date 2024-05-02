3e0ac162b40a01c05d976524450e3474e961940a
YuiSurbhi <163987279+YuiSurbhi@users.noreply.github.com>
2024-05-02 12:49:28 +0000

    Q12 commited

---
+++ Q_12.py
@@ -0,0 +1,12 @@
+# Program to generate random number.
+
+'''program using rranom module'''
+'''number 1-10'''
+import random
+num = random.randint(1,10);
+print(num);
+
+'''dice run program'''
+import random
+num = random.randint(1,6);
+print(num);
