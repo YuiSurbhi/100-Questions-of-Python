a753a9acfa32302cce3ecffd4133e373598929fa
YuiSurbhi <163987279+YuiSurbhi@users.noreply.github.com>
2024-06-08 10:35:13 +0000

    committed

--- Q_22.py
+++ Q_22.py
@@ -7,7 +7,7 @@ for i in range (1,100):
     if i % 13 == 0:
         print(i)
 
-#program using lambda and filter() funcion
+#program using lambda and filter() function
 l = [39,48,26,98,33,67,87]
 
 result = list(filter(lambda x: x % 13 == 0,l))
