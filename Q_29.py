012448672d4744d14eaf0c0c81c6b19e97dcebf0
YuiSurbhi <163987279+YuiSurbhi@users.noreply.github.com>
2024-06-26 16:29:56 +0000

    committed

---
+++ Q_29.py
@@ -0,0 +1,9 @@
+#program to Display Calendar
+
+import calendar
+
+year = int(input("Enter the year : "))
+month = int(input("Enter the month : "))
+
+calendar = calendar.month(year,month)
+print(calendar)
