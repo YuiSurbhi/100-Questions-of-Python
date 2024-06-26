cec59f74a764a9b1a9e5a4c32f67e22fbc0a64bc
YuiSurbhi <163987279+YuiSurbhi@users.noreply.github.com>
2024-06-26 16:24:17 +0000

    committed

---
+++ Q_28.py
@@ -0,0 +1,11 @@
+# program to Shuffle Deck of Cards
+
+import random, itertools
+
+deck = list(itertools.product(range(1,14),["Spade","Heart","Diamond","Club"]))
+
+random.shuffle(deck)
+print(deck)
+
+for i in range(5):
+    print(deck[i][0],"of",deck[i][1])
