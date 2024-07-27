ffc423a8c29a4675dde822206a5b43ef6eb6dd72
YuiSurbhi <163987279+YuiSurbhi@users.noreply.github.com>
2024-07-27 16:50:29 +0000

    committed

--- Q_31.py
+++ Q_31.py
@@ -1,16 +1,22 @@
 # Program to Find the Sum of Natural Numbers Using Recursion
 
-def NNS  (n):        # NNS is natural number sum
+# NNS is natural number sum
+def NNS  (n):       
+    # When n is 1 or less, just return n
     if n <= 1:
         return n
+    # Sum of current number n plus sum of numbers up to (n-1)
     else:
         return (n)+NNS(n-1)    
-    
+
+# Input from the user     
 n = int(input("Enter an number : "))
 
+# Check if number is positive 
 if n <= 0:
     print("Enter a positive number.")
 else:
+    # Call the NNS function and print the result 
     print("The sum of natural number upto given number is : ",NNS(n))
 
 
