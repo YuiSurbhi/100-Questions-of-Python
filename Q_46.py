e595eb7e4094990535ab9d4d615fddc7186f8b92
YuiSurbhi <163987279+YuiSurbhi@users.noreply.github.com>
2024-08-13 16:41:27 +0000

    committed

---
+++ Q_46.py
@@ -0,0 +1,19 @@
+# What is Slicing on List
+
+# Deine a name of list
+a = ["Mona","Ruhi","Aman","Vinesh","Geeta","Smriti"]
+
+# Prints elements from index -6 to -3 (inclusive of -6, exclusive of -3)
+print(a[-6:-3])
+# Print elements from index 1 to 4 (inclusive of 1, exclusive of 4)
+print(a[1:4])
+# Print all elements from starting index to ending index
+print(a[::])
+# Print elements from the start to index 3 (exclusive of 3)
+print(a[:3])
+# Print elements from index 2 to the end of the list
+print(a[2:])
+# Print every second element from the list
+print(a[::2])
+# Print elements from the list in reverse order
+print(a[::-1])
