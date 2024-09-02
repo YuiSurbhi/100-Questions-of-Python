884883004da0c6194179608adc535c037e328fda
YuiSurbhi <163987279+YuiSurbhi@users.noreply.github.com>
2024-09-02 16:57:19 +0000

    committed

--- Q_47.py
+++ Q_47.py
@@ -2,4 +2,19 @@
 
 friends = {"Richeal":"Ross","Monica":"Chandler","Phoebe":"Joe"}
 print(friends)
+print(friends["Monica"])
 
+# SOLUTION 1  WITH (.items)
+for key,value in friends.items():
+    print(key,"-",value)
+
+# SOLUTION 2 WITH (keys)
+for key in friends:
+    print(key,":",friends[key])
+
+# SOLUTION 3 WITH (keys and values)
+for key in friends.keys():
+    print(key)
+
+for i in friends.values():
+    print(i)
